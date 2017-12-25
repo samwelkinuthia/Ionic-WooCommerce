7be0a0348b7432827d7b8c6a2390d583ceaf475f
samwel <samwuelkinuthia@gmail.com>
2017-12-25 20:32:13 +0300

    remove list import and homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,7 @@ import { Component, ViewChild } from '@angular/core';
 import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
+import { Menu } from '../pages/menu/menu';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,7 +10,7 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = MenuPage;
+  rootPage: any = Menu;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
