fd79f73eca99368ff0224e6bd5dd86c26e504483
samwel <samwuelkinuthia@gmail.com>
2018-01-12 17:27:43 +0300

    [-UNUSED-]removed IonicPage

--- src/pages/products-by-category/products-by-category.ts
+++ src/pages/products-by-category/products-by-category.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the ProductsByCategoryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-products-by-category',
   templateUrl: 'products-by-category.html',
@@ -17,9 +9,9 @@ export class ProductsByCategoryPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad ProductsByCategoryPage');
-  }
+  //
+  // ionViewDidLoad() {
+  //   console.log('ionViewDidLoad ProductsByCategoryPage');
+  // }
 
 }
