a531da3881f141c861a1fc01ece9c221c04d3b4c
samwel <samwuelkinuthia@gmail.com>
2018-01-23 10:09:42 +0300

    require woocommerce api for reviews endpoint

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import * as WC from 'woocommerce-api';
 @Component({
   selector: 'page-product-details',
   templateUrl: 'product-details.html',
