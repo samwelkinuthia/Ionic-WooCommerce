3ab441645a23ec591586f35246624aec130610b9
samwel <samwuelkinuthia@gmail.com>
2018-01-23 10:15:50 +0300

    - Activate woocommerce with credentials - Use endpoint product/reviews to fetch reviews if any

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -9,9 +9,22 @@ import * as WC from 'woocommerce-api';
 export class ProductDetailsPage {
   product: any;
   reviews: any[];
+  WooCommerce: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.product = this.navParams.get("product");
     console.log(this.product);
+
+    this.WooCommerce = WC ({
+      url: 'URL',
+      consumerKey: 'KEY',
+      consumerSecret: 'SECRET'
+    });
+
+    this.WC.getAsync('products/' + this.product.id + '/reviews').then((data) => {
+      console.log(JSON.parse(data.body))
+    }, (err) => {
+      console.log(err)
+    });
   }
 }
