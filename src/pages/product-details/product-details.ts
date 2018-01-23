bcbc682d8bbbf408b58b16442a5408fdb487db45
samwel <samwuelkinuthia@gmail.com>
2018-01-23 10:10:27 +0300

    create empty array to hold eventual product reviews

--- src/pages/product-details/product-details.ts
+++ src/pages/product-details/product-details.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import * as WC from 'woocommerce-api';
+
 @Component({
   selector: 'page-product-details',
   templateUrl: 'product-details.html',
 })
 export class ProductDetailsPage {
   product: any;
+  reviews: any[];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.product = this.navParams.get("product");
     console.log(this.product);
