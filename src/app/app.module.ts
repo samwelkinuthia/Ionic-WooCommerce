e529b21213f6580d88a3a55fc5e877ee7fab4fd9
samwel <samwuelkinuthia@gmail.com>
2018-02-20 23:17:04 +0300

    required new page in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ProductsByCategoryPage } from "../pages/products-by-category/products-b
 import { ProductDetailsPage } from "../pages/product-details/product-details";
 import { IonicStorageModule } from "@ionic/storage";
 import { CartPage } from "../pages/cart/cart";
+import { SignupPage} from "../pages/signup/signup";
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { CartPage } from "../pages/cart/cart";
     MenuPage,
     ProductsByCategoryPage,
     ProductDetailsPage,
-    CartPage
+    CartPage,
+    SignupPage
   ],
   imports: [
     BrowserModule,
@@ -32,7 +34,8 @@ import { CartPage } from "../pages/cart/cart";
     MenuPage,
     ProductsByCategoryPage,
     ProductDetailsPage,
-    CartPage
+    CartPage,
+    SignupPage
   ],
   providers: [
     StatusBar,
