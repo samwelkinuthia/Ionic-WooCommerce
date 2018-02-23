c8bfa59f14c19a0ab6c11297e4e5eaef88ebfa85
samwel <samwuelkinuthia@gmail.com>
2018-02-23 19:01:10 +0300

    check user email against regex

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -33,7 +33,11 @@ export class SignupPage {
     let validEmail = false;
 
     let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    
+
+    if (reg.test(this.newUser.email)) {
+
+    }
+
   }
 
 
