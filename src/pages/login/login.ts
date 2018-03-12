c902ba6a38ac6a7b3ccb3e062572be845cd6af5f
samwel <samwuelkinuthia@gmail.com>
2018-03-12 14:31:47 +0300

    successful check for user in woocommerce

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from "@angular/common/http";
+import { Storage } from "@ionic/storage";
 
 @Component({
   selector: 'page-login',
@@ -11,7 +12,7 @@ export class LoginPage {
   username: string;
   password: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public http: HttpClient, public storage: Storage) {
 
     this.username = '';
     this.password = '';
@@ -20,6 +21,11 @@ export class LoginPage {
 
   login() {
 
+    this.http.get('http://www.creative-junk.com/storedemo/api/auth/generate_auth_cookie/?insecure=cool&username=' + this.username + '&password=' + this.password).subscribe((response) => {
+
+      console.log(response);
+      
+    })
   }
 
 
