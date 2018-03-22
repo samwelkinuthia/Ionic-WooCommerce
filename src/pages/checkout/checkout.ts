63e3dd6419d7ec9bde31e349cf2b4fbc0b6b1892
samwel <samwuelkinuthia@gmail.com>
2018-03-22 22:35:26 +0300

    clean ionicpage

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the CheckoutPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-checkout',
   templateUrl: 'checkout.html',
@@ -16,10 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class CheckoutPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad CheckoutPage');
   }
 
 }
