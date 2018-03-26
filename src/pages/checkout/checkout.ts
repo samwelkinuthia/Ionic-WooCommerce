a77a61e51d8ad12db9bf2523543f736f2846bbfd
samwel <samwuelkinuthia@gmail.com>
2018-03-26 09:19:09 +0300

    on click set similar shipping and billing

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -7,7 +7,36 @@ import { NavController, NavParams } from 'ionic-angular';
 })
 export class CheckoutPage {
 
+  newOrder: any;
+  paymentMethods: any;
+  paymentMethos: any;
+  similar: boolean;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+
+    this.newOrder = {};
+    this.newOrder.billing_address = {};
+    this.newOrder.shippingAddress = {};
+    this.similar = false;
+
+    this.paymentMethos = [
+      {method_id: 'bacs', method_title: 'Direct Bank Transfer'},
+      {method_id: 'cheque', method_title: 'Cheque'},
+      {method_id: 'cod', method_title: 'Cash On Delivery'},
+      {method_id: 'paypal', method_title: 'Paypal'}
+    ];
+
+  }
+
+  setSimilar() {
+
+    this.similar = !this.similar;
+
+    if (this.similar) {
+
+      this.newOrder.billing_address = this.newOrder.shippingAddress
+
+    }
   }
 
 }
