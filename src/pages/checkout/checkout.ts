1d62a2fcf9639d5ab38d06cda9f48290060829cf
samwel <samwuelkinuthia@gmail.com>
2018-03-30 13:14:14 +0300

    paypal payment code

--- src/pages/checkout/checkout.ts
+++ src/pages/checkout/checkout.ts
@@ -103,6 +103,47 @@ export class CheckoutPage {
 
       //NEXT UP
 
+      this.payPal.init({
+        PayPalEnvironmentProduction: 'YOUR_PRODUCTION_CLIENT_ID',
+        PayPalEnvironmentSandbox: 'YOUR_SANDBOX_CLIENT_ID'
+      }).then(() => {
+        // Environments: PayPalEnvironmentNoNetwork, PayPalEnvironmentSandbox, PayPalEnvironmentProduction
+        this.payPal.prepareToRender('PayPalEnvironmentSandbox', new PayPalConfiguration({
+          // Only needed if you get an "Internal Service Error" after PayPal login!
+          //payPalShippingAddressOption: 2 // PayPalShippingAddressOptionPayPal
+        })).then(() => {
+          let payment = new PayPalPayment('3.33', 'USD', 'Description', 'sale');
+          this.payPal.renderSinglePaymentUI(payment).then(() => {
+            // Successfully paid
+
+            // Example sandbox response
+            //
+            // {
+            //   "client": {
+            //     "environment": "sandbox",
+            //     "product_name": "PayPal iOS SDK",
+            //     "paypal_sdk_version": "2.16.0",
+            //     "platform": "iOS"
+            //   },
+            //   "response_type": "payment",
+            //   "response": {
+            //     "id": "PAY-1AB23456CD789012EF34GHIJ",
+            //     "state": "approved",
+            //     "create_time": "2016-10-03T13:33:33Z",
+            //     "intent": "sale"
+            //   }
+            // }
+          }, () => {
+            // Error or render dialog closed without being successful
+          });
+        }, () => {
+          // Error in configuration
+        });
+      }, () => {
+        // Error in initialization, maybe PayPal isn't supported or something else
+      });
+
+
     } else {
 
       this.storage.get('cart').then((cart) => {
