44020016787a13d57ec5ac1c5bed9872659974f5
samwel <samwuelkinuthia@gmail.com>
2018-04-05 18:00:57 +0300

    search functionality using woocommerce

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,25 +1,65 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams, ToastController } from 'ionic-angular';
+import {WoocommerceProvider} from "../../providers/woocommerce/woocommerce";
+import {ProductDetailsPage} from "../product-details/product-details";
 
-/**
- * Generated class for the SearchPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html',
 })
 export class SearchPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  searchQuery: string = "";
+  WooCommerce: any;
+  products: any[] = [];
+  // page: number;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private WP: WoocommerceProvider, public toastCtrl: ToastController) {
+
+    this.WooCommerce = WP.init();
+
+    console.log(this.navParams.get('searchQuery'));
+
+    this.searchQuery = this.navParams.get('searchQuery');
+
+    this.WooCommerce.getAsync('products?filter[q]='+ this.searchQuery).then((data) => {
+
+      this.products = JSON.parse(data.body).products;
+
+      console.log(this.products)
+
+    })
+
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchPage');
+  showProduct(product){
+    this.navCtrl.push(ProductDetailsPage, {"product": product});
   }
+
+  // loadMoreProducts(event) {
+  //
+  //   this.WooCommerce.getAsync('products?filter[q]='+ this.searchQuery + '&page=' + this.page).then((data) => {
+  //
+  //     this.products = this.products.concat(JSON.parse(data.body).products);
+  //
+  //     if (JSON.parse(data.body).products.length < 10 ) {
+  //
+  //       event.enable(false);
+  //
+  //       this.toastCtrl.create({
+  //         message: 'Ooooops, No more products',
+  //         duration: 3000
+  //       }).present();
+  //
+  //     }
+  //
+  //     event.complete();
+  //     this.page ++;
+  //
+  //   })
+  //
+  // }
+
+
 
 }
